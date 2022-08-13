674a4ef8c5cd1aa473f9a84ac6cb8d902404077b
Shon <shonpazarker@microsoft.com>
2022-08-14 00:55:21 +0300

    phase-3-references

--- src/App.tsx
+++ src/App.tsx
@@ -17,36 +17,36 @@ const Counter = styled.p`
   color: white;
 `;
 
+const charactersData = [
+  {
+    id: 1,
+    image: rickImg,
+    name: "Rick Sanchez",
+    location: { name: "Citadel of Ricks" },
+    origin: { name: "Earth (C-137)" },
+  },
+  {
+    id: 2,
+    image: mortyImg,
+    name: "Morty Smith",
+    location: { name: "Citadel of Ricks" },
+    origin: { name: "Earth" },
+  },
+  {
+    id: 5,
+    image: pickleRickImg,
+    name: "Pickle Rick",
+    location: { name: "Citadel of Ricks" },
+    origin: { name: "Earth (C-137)" },
+  },
+];
+
 function App() {
   const [counter, setCounter] = useState(0);
   const onIncreaseCounter = useCallback(
     () => setCounter((prevCounter) => prevCounter + 1),
     []
   );
-
-  const charactersData = [
-    {
-      id: 1,
-      image: rickImg,
-      name: "Rick Sanchez",
-      location: { name: "Citadel of Ricks" },
-      origin: { name: "Earth (C-137)" },
-    },
-    {
-      id: 2,
-      image: mortyImg,
-      name: "Morty Smith",
-      location: { name: "Citadel of Ricks" },
-      origin: { name: "Earth" },
-    },
-    {
-      id: 5,
-      image: pickleRickImg,
-      name: "Pickle Rick",
-      location: { name: "Citadel of Ricks" },
-      origin: { name: "Earth (C-137)" },
-    },
-  ];
 
   return (
     <Root className="characters">
